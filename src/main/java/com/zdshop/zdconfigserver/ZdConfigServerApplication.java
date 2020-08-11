2a6c4ad7ae67bb458591da589071849b67401393
Aghilas DJEMA <aghilas@MBP-de-Aghilas.home>
2020-08-11 22:49:58 +0200

    first commit

---
+++ src/main/java/com/zdshop/zdconfigserver/ZdConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.zdshop.zdconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZdConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZdConfigServerApplication.class, args);
+    }
+
+}
