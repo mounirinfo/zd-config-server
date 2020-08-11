bcd22f077a0739293be21b1b61bed4644c5c0e0b
Mounir ZAGHBA <zaghba.mounir@gmail.com>
2020-08-11 23:12:02 +0200

    update config server to communicate with git config

--- src/main/java/com/zdshop/zdconfigserver/ZdConfigServerApplication.java
+++ src/main/java/com/zdshop/zdconfigserver/ZdConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.zdshop.zdconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ZdConfigServerApplication {
 
     public static void main(String[] args) {
