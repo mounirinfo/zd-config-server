c0f37dbc8c139752a745bbd20f51acb8cb47bb62
Aghilas DJEMA <aghilas@mbp-de-aghilas.home>
2020-08-26 21:04:46 +0200

    edit sh file

--- run-config-server.sh
+++ run-config-server.sh
@@ -1,4 +1,13 @@
+#start app on docker container
 
+#shoose java version
+export JAVA_HOME=`/usr/libexec/java_home -v 14`
 
-#docker build -t zd/zd-config-server:0.0.1 .
-#docker run --rm -d -p 9091:9091 zd/zd-config-server:0.0.1
+#build JAR file
+mvn clean install
+
+#build docker image
+docker build -t zd/zd-config-server:0.0.1 .
+
+#start docker container
+docker run --rm -d -p 9091:9091 zd/zd-config-server:0.0.1
